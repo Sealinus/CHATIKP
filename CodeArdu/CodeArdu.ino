18568e49d931c03e40a4eefe596c2493c68c227e
Sealinus <kiril.l.prigozhin@gmail.com>
2022-12-08 12:58:58 +0300

    Completed Object Central Key

--- CodeArdu/CodeArdu.ino
+++ CodeArdu/CodeArdu.ino
@@ -3,19 +3,19 @@
 #define txp 2
 #define rxp 12
 #define sone 5
-#define stwo 6
+#define stwo 6    // задаем порты
 #define keyp 7
 
 #include <SimpleKeypad.h>
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>  //Подключаем либы
 #include <SimpleKeypad.h>
 #include <Gyver433.h>
 #include <EncButton2.h>
 
 
 static byte colPins[KP_COLS] = { 11, 10, 9, 8 };
-static byte rowPins[KP_ROWS] = { A0, A1, A2, A3 };
+static byte rowPins[KP_ROWS] = { A0, A1, A2, A3 }; //Глобальные переменнвые
 static char keys[KP_ROWS][KP_COLS] = {
   { '1', '2', '3', 'A' },
   { '4', '5', '6', 'B' },
@@ -25,7 +25,7 @@ static char keys[KP_ROWS][KP_COLS] = {
 SimpleKeypad pad((char*)keys, rowPins, colPins, KP_ROWS, KP_COLS);
 
 Gyver433_TX<txp> tx;
-
+                                                          //создаем объекты
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 EncButton2<EB_ENCBTN> enc(INPUT, sone, stwo, keyp);  // энкодер с кнопкой
@@ -34,7 +34,7 @@ String msg = "";
 
 
 void setup() {
-  lcd.init();
+  lcd.init(); //дисплей и сериал
   lcd.backlight();
   Serial.begin(9600);
 }
@@ -42,10 +42,7 @@ void setup() {
 void loop() {
   static int channel = 0;
   static char key = pad.getKey();
-  static char NextChar = "";
-  static int RadioChan = 0;
-  static String TextOnScreen = "";
-  static String NumOnScreen = "";
+
   key = keyboard();
   if (pad.getKey() == 'A') tx.sendData(key);
   
